681dd5633a0afa616742fdf73ea0de8327c425e5
Rahul Passi <rahulpassi2412@gmail.com>
2018-02-25 23:06:35 +0530

    Add files via upload

---
+++ format/main.java
@@ -0,0 +1,17 @@
+package format;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double x=6;
+System.out.println(String.format("%.2f",x));
+System.out.printf("%.3f\n",x);
+float g=3.5f;
+System.out.println("\t"+g);
+double k=(int)g;
+System.out.println("\t"+k);
+
+	}
+
+}
